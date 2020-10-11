d7586c2ec941146980c4c8ce2fcbd162e2d1695e
Sulemana Masahudu <62577421+SulemanaMasahudu@users.noreply.github.com>
2020-10-11 15:12:23 +0100

    Update settings.py

--- offshore/settings.py
+++ offshore/settings.py
@@ -39,12 +39,18 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'aboutuser',
     'rest_framework',
-    'frontend'
+    'frontend',
+    'corsheaders',
+]
+
+CORS_ORIGIN_WHITELIST = [
+    'http://localhost:3000'
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
