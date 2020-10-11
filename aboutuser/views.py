ba5c97935f568b0266fbdf5a7db31fee6c037428
Sulemana Masahudu <62577421+SulemanaMasahudu@users.noreply.github.com>
2020-10-11 15:07:07 +0100

    Update views.py

--- aboutuser/views.py
+++ aboutuser/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from .serializers import AboutUserSerializer
+from .models import Person
+from django.http import HttpResponse
+
+
 
 # Create your views here.
+class Aboutuserviewset(viewsets.ModelViewSet):
+    serializer_class = AboutUserSerializer
+    queryset = Person.objects.all()
+
